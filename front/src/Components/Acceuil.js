ca549e8eb405d5b410b0ec67502794ec1aa2f8a4
julian <julian.manchec@eipitech.eu>
2022-11-15 11:25:20 +0100

    map maj

--- front/src/Components/Acceuil.js
+++ front/src/Components/Acceuil.js
@@ -10,7 +10,7 @@ function Acceuil() {
 
     return (
         <>
-            <Search />
+            <Map />
         </>
     )
 
---
+++ front/src/Components/Map.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect } from 'react';
+
+function Map()
+{
+
+    var axiosConfig = {
+        method: 'get',
+        url: "https://api.mapbox.com/directions/v5/mapbox/cycling/-122.42,37.78;-77.03,38.91?access_token=pk.eyJ1IjoianVsaWFubWFuY2hlYyIsImEiOiJjbGFpMGkwaXAwaDE5M3dtejY1aXVmZzZoIn0.xxr5qSlakNCcOmRQh9RKMg",
+        headers: {}
+    };
+
+    useEffect(() => 
+    {
+        axios(axiosConfig).then(function(response) 
+        {
+            console.log(response.data);
+        }).catch(function (error) 
+        {
+            console.log(error);
+        });
+    });
+
+    return (
+        <img src="https://api.mapbox.com/styles/v1/mapbox/light-v10/static/-87.0186,32.4055,14/500x300?access_token=pk.eyJ1IjoianVsaWFubWFuY2hlYyIsImEiOiJjbGFpMGkwaXAwaDE5M3dtejY1aXVmZzZoIn0.xxr5qSlakNCcOmRQh9RKMg" alt="test"/>
+    )
+}
+
+export default Map;
